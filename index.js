76a660cce4030f4355b23357608c39dbe9f721a7
samwaseee <samwaseee@gmail.com>
2024-05-13 10:48:50 +0600

    database review issue solved

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         //await client.connect();
 
         const roomCollection = client.db('BookedInn').collection('rooms');
